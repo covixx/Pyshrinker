5663b3c23ee1288de6d9861deaf41697324dff77
V <75623407+covixx@users.noreply.github.com>
2024-08-15 13:57:04 +0530

    Update libfinder.py

--- libfinder.py
+++ libfinder.py
@@ -1,36 +1,28 @@
 import os
 import re
-import importlib.util
 
-def get_libs(folder_path):
-    # Traverse the folder recursively
+def get_modules(getpath):
+    with open(getpath, 'r') as f:
+        lines = f.readlines()
+
+    import_pattern = re.compile(r'^(?:from\s+([\w.]+)\s+import\s+[\w,\s*]+|import\s+([\w.,\s]+))$')
+
     libraries = set()
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.py'):
-                file_path = os.path.join(root, file)
+    for line in lines:
+        line = line.strip()
+        match = import_pattern.match(line)
+        if match:
+            if match.group(1):  # 'from' import
+                lib = match.group(1).split('.')[0]
+                libraries.add(lib)
+            else:  # 'import' statement
+                imports = match.group(2).split(',')
+                for imp in imports:
+                    lib = imp.strip().split(' as ')[0].split('.')[0]
+                    libraries.add(lib)
 
-                with open(file_path, 'r') as f:
-                    lines = f.readlines()
-
-                import_pattern = re.compile(r'^(?:from\s+([\w.]+)\s+import\s+[\w,\s*]+|import\s+([\w.,\s]+))$')
-
-    
-                for line in lines:
-                    line = line.strip()
-                    match = import_pattern.match(line)
-                    if match:
-                        if match.group(1):  # 'from' import
-                            lib = match.group(1).split('.')[0]
-                            if importlib.util.find_spec(lib) is not None:
-                                libraries.add(lib)
-                        else:  # 'import' statement
-                            imports = match.group(2).split(',')
-                            for imp in imports:
-                                lib = imp.strip().split(' as ')[0].split('.')[0]
-                                if importlib.util.find_spec(lib) is not None:
-                                    libraries.add(lib)
-
-    
     sorted_libraries = sorted(libraries)
+    print(sorted_libraries)
     return sorted_libraries
+
+get_modules('C:\\Users\\Vaibhav\\Desktop\\screensaver.py')
